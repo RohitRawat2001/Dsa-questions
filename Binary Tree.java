ad089fd22167c8b885e5e6931fdee90a20812305
Rohit Rawat <89390966+RohitRawat2001@users.noreply.github.com>
2023-12-02 19:11:02 +0530

    Update Binary Tree.java

--- Binary Tree.java
+++ Binary Tree.java
@@ -1,27 +1,27 @@
-// public class Binary Tree {
-//     class Node{
-//         int data;
-//         Node left,right;
+public class Binary Tree {
+    class Node{
+        int data;
+        Node left,right;
 
-//         Node(int data){
-//             this.data=data;
-//             this.left=null;
-//             this.right=null;       
-//         }
-//         void Node insert(Node root,int data){
-//              if(root == null)
-//              return newNode(data);
-//             if(data>root.data){
-//                  root.right=insert(root.right,data);
-//              }
-//              else if(data<root.data){
-//                  root.left=insert(root.left,data);
-//              }
+        Node(int data){
+            this.data=data;
+            this.left=null;
+            this.right=null;       
+        }
+        void Node insert(Node root,int data){
+             if(root == null)
+             return newNode(data);
+            if(data>root.data){
+                 root.right=insert(root.right,data);
+             }
+             else if(data<root.data){
+                 root.left=insert(root.left,data);
+             }
 
-//             }
-//     }
-//     public static void main(String[] args) {}
+            }
+    }
+    public static void main(String[] args) {}
         
-//     }
+    }
     
-// }
+}
